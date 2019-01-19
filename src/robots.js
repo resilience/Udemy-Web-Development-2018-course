e79b52c225b331ec4668c9e3aac22748a0598431
resilience <dmaree2@gmail.com>
2019-01-19 22:16:59 +0200

    previous end point

---
+++ src/robots.js
@@ -0,0 +1,36 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'RoboDummy',
+		username: 'Bret',
+		email: 'Applepie@sky.air'
+	},
+
+	{
+		id: 2,
+		name: 'Harry',
+		username: 'Trueman',
+		email: 'htrueman@show.biz'
+
+	},
+	{
+		id: 3,
+		name: 'Harriet',
+		username: 'clemence',
+		email: 'Harry@calamity.org'
+
+	},
+	{
+		id: 4,
+		name: 'Hall Jordan',
+		username: 'orangat',
+		email: 'orange@utan.ape'
+	},
+	{
+		id: 5,
+		name: 'piggysue',
+		username: 'patty',
+		email: 'fried@patty.mcrib'
+	}
+
+];
